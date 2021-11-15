40f252214f21c1de00428bc6737bad78f372514e
SangWon Park <sangwonpark4@gmail.com>
2021-11-14 22:43:05 -0500

    Edit 2 - SangWon (need to figure out how to authenticate spotify)

--- Feelingsfy/Feelingsfy/AppDelegate.swift
+++ Feelingsfy/Feelingsfy/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "fKWV41v4Dgnmz7MMB0sf8nATunmZxY8JtnhXvmqZ"
+            $0.clientKey = "uAGIS26XmPXzbMrSHRCFfgUXuU7zATiP5qnpTP23"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
--- Feelingsfy/Feelingsfy/LoginViewController.swift
+++ Feelingsfy/Feelingsfy/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 class LoginViewController: UIViewController {
 
@@ -15,15 +17,27 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
     }
-    */
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+            
+        }
+    }
+    
+   
 
 }
